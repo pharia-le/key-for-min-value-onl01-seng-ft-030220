cbbcbf5f976add70c6b30abb10cb4fe31ef7ee4e
Pharia Le <pharia.le@gmail.com>
2020-03-05 17:17:33 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lowest = ""
+  new_key = []
+  
+  
+  name_hash.map do |k, v|
+    if lowest == '' || lowest.to_i > v
+      lowest = v.to_s
+      new_key[0] = k
+    end
+  end
+  new_key[0]
 end
